0d8d6472f0ea88c0e0faf3fc3939352a860a3344
Teaga2020 <sand_bun2007@yahoo.co.jp>
2020-11-25 17:07:33 +0900

    投稿詳細ページの実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -16,6 +16,23 @@ class PrototypesController < ApplicationController
       render :new
     end
   end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
   
   private
   def prototype_params
