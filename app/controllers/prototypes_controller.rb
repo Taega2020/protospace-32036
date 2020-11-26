3c2916e5f87eaf7ba66887e6a210a1d2f6196065
Teaga2020 <sand_bun2007@yahoo.co.jp>
2020-11-26 17:10:47 +0900

    ユーザー制限実装後の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :update]
+
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -26,6 +26,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
@@ -47,10 +50,4 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-  def move_to_index
-    unless current_user.id == params[:id].to_i
-      redirect_to action: :index
-    end
-  end
-
 end
